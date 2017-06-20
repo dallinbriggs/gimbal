c83f08deafe0c8bde341a5d84e258b5eb7ca2074
Dallin Briggs <dallinbriggs@gmail.com>
2017-06-20 14:34:54 -0600

    Changed state message origin to ros plane

--- scripts/gimbal_geopointer.py
+++ scripts/gimbal_geopointer.py
@@ -8,8 +8,8 @@ import tf
 # import utilities
 
 from geometry_msgs.msg import Vector3, Vector3Stamped
-from rosflight_msgs.msg import State, GPS
-from ros_plane.msg import Current_Path, Controller_Commands
+from rosflight_msgs.msg import GPS
+from ros_plane.msg import State, Current_Path, Controller_Commands
 from nav_msgs.msg import Odometry
 # from inertial_sense.msg import GPS
 # from rosflight_msgs.msg import Attitude
