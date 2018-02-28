175570199d1a779d530fe5876c3a68fab104a174
dallinbriggs <dallinbriggs@gmail.com>
2018-02-27 18:56:08 -0700

    started package

---
+++ include/gimbal_serializer.h
@@ -0,0 +1,11 @@
+#ifndef GIMBAL_SERIALIZER_H
+#define GIMBAL_SERIALIZER_H
+
+
+class gimbal_serializer
+{
+public:
+    gimbal_serializer();
+};
+
+#endif // GIMBAL_SERIALIZER_H
---
+++ src/gimbal_serializer.cpp
@@ -0,0 +1,6 @@
+#include "gimbal_serializer.h"
+
+gimbal_serializer::gimbal_serializer()
+{
+
+}
